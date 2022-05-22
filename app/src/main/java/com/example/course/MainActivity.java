47cd0c8be7cb782246db072bc078a2f627732d90
Hamza EL Yousfi <Hamza.El_Yousfi@bordeaux-inp.fr>
2022-05-22 13:29:35 +0200

    First commit

--- app/src/main/java/com/example/course/MainActivity.java
+++ app/src/main/java/com/example/course/MainActivity.java
@@ -6,11 +6,13 @@ import android.content.SyncStatusObserver;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView text;
+    Button btn;
     int i=0;
 
     @Override
@@ -37,7 +39,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        
+        // Change text value using button
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(i % 2 == 0)
+                {
+                text.setText("Button 1 Clicked!");
+                }
+                else
+                {
+                    text.setText("Button 1 uncklicked");
+                }
+                i = i + 1;
+
+            }
+        });
+
+
 
 
 
